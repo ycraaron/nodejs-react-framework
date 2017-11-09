a5087498e48923cdb713786801920085edfb2b60
Aaron Yang <ycr_aaron@foxmail.com>
2017-11-10 05:06:26 +0800

    Add googlemap nodejs client.

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,6 +1,57 @@
 // import {Zone} from '../models/Zone'
 // controller are meant to execute specific to the resources
+
 let Movie = require('../models/Movie')
+
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyDKHXaaZZmXiTBx_jIXupCXZavq06jT614'
+  });
+
+
+function saveSingleMovie(movie, callback){
+    let singleMovie = 
+    {
+        actor_1:'',
+        actor_2:'',
+        actor_3:'',  
+        director:'',
+        locations:[],
+        production_company:'', 
+        release_year:'', 
+        title:'',
+        writer:''
+    }
+    for(let key in movie){
+        let value = movie[key]
+        if(key == 'locations'){
+            if(value == '')
+                continue
+        }
+        else{
+            singleMovie[key] = value
+        }
+        // console.log(singleMovie)
+        let address = movie['locations']
+        // console.log(value)    
+        if(address != ''){
+            googleMapsClient.geocode({
+                address: address
+                }, function(err, response) {
+                if (!err) {
+                    result = response.json.results;
+                    lat = result[0].geometry.location.lat
+                    lng = result[0].geometry.location.lng
+                    let locations = [{address:address, coord:{lat:lat, lng:lng}}]
+                    singleMovie['locations'] = locations
+                    console.log('callback in load single movie')
+                    console.log(singleMovie)
+                    callback(singleMovie)
+                }
+            });
+        }
+    }
+//console.log(singleMovie)
+}
 
 module.exports = {
     
@@ -34,26 +85,19 @@ module.exports = {
     },
 
     saveMovieToMongo : function(movie, callback){
-        
-        let zips = params['zipCodes']
-        let zip = zips.split(',')
-        let newZips = []
-        
-        zip.forEach(function(zipCode){
-            newZips.push(zipCode.trim())
-        })
-
-        params['zipCodes'] = newZips
-
-        console.log('creating movie')
-        Movie.create(doc, function(err, result){
-            if (err){
-                callback(err, null)
-                return
-            }
-            else
-                callback(null, result)
+        saveSingleMovie(movie, function(result){
+            movie = result        
+            Movie.create(movie, function(err, result){
+                if (err){
+                    console.log('error', err)
+                    callback('error', null)
+                }
+                callback(null,'inserted')
+            })
         })
     }
 
+}
+function newFunction() {
+    return { type: String, default: '' };
 }
